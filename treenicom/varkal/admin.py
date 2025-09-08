a3f119ef1a7931e7087e2d12847c5340c9710633
Renne Jämsen <renne@renne.rev.dnainternet.fi>
2025-09-08 15:16:48 +0300

    Revert "Re-init"
    This reverts commit 5f3952bf880208c376a7e5c8f8c853f3e7fa2ac6.

--- treenicom/varkal/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Service)
-admin.site.register(Place)
-admin.site.register(TimeSlot)
-admin.site.register(Reservation)
-admin.site.register(Customer)
